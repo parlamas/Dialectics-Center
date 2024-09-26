cd82307ffb05235f63d5c9c1b6feae232fd8fe73
parlamas <parlamas@live.com>
2024-09-26 22:33:24 +0200

    2nd-try

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,3 +1,4 @@
+//src/app/sign-in/[[...sign-in]]/page.tsx
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,3 +1,4 @@
+//src/app/sign-up/[[...sign-up]]/page.tsx
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
